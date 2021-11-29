f8da6a5677e535b2a2ecab44c62c60c5a33a2c01
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-11-29 04:20:29 +0300

    fix bug in ValueError: Field 'id' expected a number but got 'site'.

--- catchy/models.py
+++ catchy/models.py
@@ -32,7 +32,7 @@ class Image(models.Model):
     
     @classmethod
     def search_by_category(cls,search_term):
-        photos = cls.objects.filter(category=search_term)
+        photos = cls.objects.filter(category__category=search_term)
         return photos
 
     @classmethod
